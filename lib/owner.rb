f56901d84fbf2189cc870bbdc1d258a744a0c31f
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-15 17:23:09 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
   # code goes here
+  attr_accessor :species
+
+
+
+  @@all = []
+
+  def initialize(species)
+
+    @species = species
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all =[]
+  end
+
+  def self.count
+    @@all.length
+  end
+
+
+
 end
