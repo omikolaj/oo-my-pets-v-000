95e241f408fe36b0ba0da9ba6fbdf51559876277
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-16 08:45:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,12 @@
 class Owner
   # code goes here
-  attr_reader :species, :name
-
-
+  attr_accessor :name
+  attr_reader :species
 
   @@all = []
 
   def initialize(species)
-
     @species = species
-
     @@all << self
   end
 
